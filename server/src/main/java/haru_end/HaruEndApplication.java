34fee1daf26fc25a7a8fa59fee659d29413ee079
silverclean091 <wjdghkdidi@naver.com>
2024-08-12 00:08:01 +0900

    [Feature]: spring boot 개발환경 구축

---
+++ server/src/main/java/haru_end/HaruEndApplication.java
@@ -0,0 +1,13 @@
+package haru_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaruEndApplication.class, args);
+	}
+
+}
