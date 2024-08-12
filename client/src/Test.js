6cb9b09f4d5b3c3a94912faee74d2b096045a20e
hayeon02 <jhy414800@cu.ac.kr>
2024-08-13 00:45:47 +0900

    [Test]:빈화면에 text 출력

---
+++ client/src/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Test() {
+    return(
+        <div className="test">
+            <h2>Hello</h2>
+            <div>출력 완료</div>
+        </div>
+    );
+}
+
+export default Test;
