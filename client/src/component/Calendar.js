6deca4f95a5844aea039a59060434ef2c4c7fe42
so365247 <antk9129@naver.com>
2024-08-18 20:39:58 +0900

    [Feature]: 달력 요일 표기 수정

--- client/src/component/Calendar.js
+++ client/src/component/Calendar.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-export default function App() {
+function App() {
     const [value, onChange] = useState(new Date());
 
     return (
         <div>
-            <Calendar onChange={onChange} value={value} />
+            <Calendar
+            onChange={onChange} value={value}
+            calendarType='hebrew'
+            />
         </div>
     );
 }
+
+export default App;
