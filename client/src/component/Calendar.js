0aee58617af0d064a075c24f2d829d5310fc3bd1
so365247 <antk9129@naver.com>
2024-08-20 05:00:42 +0900

    [Chore]: 섹션 나누기

--- client/src/component/Calendar.js
+++ client/src/component/Calendar.js
@@ -2,22 +2,45 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import '../style/Calendar.css';
 
-function App() {
+function Cal() {
     const [value, onChange] = useState(new Date());
 
     return (
         <div>
-            <Calendar
-            onChange={onChange} value={value}
-            calendarType='gregory'
-            formatDay={(locale, date) => date.toLocaleString('en', 
-                {day: 'numeric'})}
-            prev2Label={null}
-            next2Label={null}
-            showNeighboringMonth={false}
-            />
+            <div class='top'>
+                <div class='menu'>
+                    메뉴 아이콘 자리
+                </div>
+                <div class='alert'>
+                    알림 아이콘 자리
+                </div>
+            </div>
+            <div class='mid'>
+                <div class='today'>
+                    오늘 날짜 자리
+                </div>
+                <div class='cal'>
+                    <Calendar
+                    onChange={onChange} value={value}
+                    calendarType='gregory'
+                    formatDay={(locale, date) => date.toLocaleString('en', 
+                        {day: 'numeric'})}
+                    prev2Label={null}
+                    next2Label={null}
+                    showNeighboringMonth={false}
+                    />
+                </div>
+                <div class='schedule'>
+                    일정
+                </div>
+            </div>
+            <div class='under'>
+                <div class='foot'>
+                    하단바
+                </div>
+            </div>
         </div>
     );
 }
 
-export default App;
+export default Cal;
